0157303430df1a3028f4985efc2f7ff6dc4f50b2
Salix <ycw2416@outlook.com>
2023-09-15 18:59:36 +0800

    new

--- setup_imgs.py
+++ setup_imgs.py
@@ -1,12 +1,33 @@
-import bs4 as bs
+from bs4 import BeautifulSoup
 
 
-class Infomation:
-    def __init__(self):
-        pass
+class Infomation(BeautifulSoup):
+    def __init__(self, name: str, html_context: str):
+        super().__init__(html_context, "html5lib")
+        self.name = name
 
-    def generate_images(self):
-        pass
+    @classmethod
+    def read_local(cls, path: str):
+        with open(path, 'r', encoding='utf-8') as f:
+            context = f.read()
+            s = path.rfind("/")
+            e = path.rfind(".")
+            name = path[s + 1: e]
+            return cls(name,  context)
 
-    def change_img_url(self):
-        pass
+    def change_images(self, prefix: str):
+        for i, img in enumerate(self.find_all("img")):
+            print(type(img.attrs["style"]), img.attrs["style"])
+            extend = str(img.attrs["src"])
+            extend = extend[extend.rfind("."):]
+            img.attrs["src"] = "{}{}-{}{}".format(prefix, self.name, i, extend)
+
+    def show_images(self):
+        for img in self.find_all("img"):
+            print(img.attrs)
+
+
+if __name__ == '__main__':
+    a = Infomation.read_local("format_html/Abducens_nerve.html")
+    a.change_images("../infomation/")
+    a.show_images()
